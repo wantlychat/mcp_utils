f9caf1fff1ebfc406a5187dd27ec5103f9dc3890
Michael Groys <mgroys@gmail.com>
2025-10-16 18:48:14 +0300

    Adding @mcp_initializer

--- src/mcp_utils/__init__.py
+++ src/mcp_utils/__init__.py
@@ -1,3 +1,4 @@
 from .mcp_creator import create_protected_mcp
+from .mcp_initializer import mcp_initializer, get_mcp_initializers, is_mcp_initializer
 
-__all__ = ["create_protected_mcp"]
+__all__ = ["create_protected_mcp", "mcp_initializer", "get_mcp_initializers", "is_mcp_initializer"]
